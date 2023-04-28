af76ca4d1a545157dd19047fdcda208b09766316
SanketNavghane <snavghane393@gmail.com>
2023-04-28 13:42:02 +0530

    six commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,39 @@
-import { Component } from '@angular/core';
+
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'reactive-form';
+
+export class AppComponent implements OnInit {
+
+  Loginform!:FormGroup;
+
+  constructor(private fb:FormBuilder){
+      
+  }
+
+  ngOnInit(): void {
+  this.Loginform = this.fb.group(
+    {
+      FirstName:[''],
+      LastName:[''],
+      MobileNumber:[''],
+      Email:[''],
+      Password:['']
+  }
+  )
+
+  }
+
+  signin(frm:any){
+    
+    console.log(frm.value);
+    console.log(frm)
+
+    
+  }
 }
